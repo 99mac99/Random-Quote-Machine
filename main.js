bd8378f5ab0996e009aa5d09720f98e960eabb64
99mac99 <86948705+99mac99@users.noreply.github.com>
2021-09-10 15:58:01 +0200

    dodanie api

--- main.js
+++ main.js
@@ -1,59 +1,61 @@
 const newQuoteButton = document.querySelector('.button');
 const quoteText = document.querySelector('.quote-text');
-const quoteAuthor = document.querySelector('.quote-author');
-const colorTextChanger = document.querySelector('.button')
-const colorChanger = document.querySelector('.button')
+const quoteAuthor = document.querySelector('.quote-author')
 
 
-const quotes = [
-	'Genius is one percent inspiration and ninety-nine percent perspiration.',
-	'A house divided against itself cannot stand.',
-	'Difficulties increase the nearer we get to the goal.',
-	'Fate is in your hands and no one elses',
-	'Be the chief but never the lord.',
-	'Nothing happens unless first we dream.',
-	'Well begun is half done.',
-	'Life is a learning experience, only if you learn.',
+const apiLink ='https://game-of-thrones-quotes.herokuapp.com/v1/random'
 
-]
+const getQuote = () => {
+	url = apiLink
 
-
-
-const showRandomQuotes = () => {
-	const number = Math.floor(Math.random() * (quotes.length - 1));
-	console.log(number);
-	quoteText.textContent = quotes[number];
-
-};
-
-const colorChange = () => {
-const r = Math.floor(Math.random() * 255)
-const g = Math.floor(Math.random() * 255)
-const b = Math.floor(Math.random() * 255)
-colorChanger.style.background = `rgb(${r},${g},${b})`
-}
-
-const colorTextChange = () => {
-const r = Math.floor(Math.random() * 255)
-const g = Math.floor(Math.random() * 255)
-const b = Math.floor(Math.random() * 255)
-colorTextChanger.style.color= `rgb(${r},${g},${b})`
+	axios.get(url)
+	.then(res =>console.log(res))
 }
 
 
 
 
-
-// const colorTextChange = () => {
-// const r = Math.floor(Math.random() * 255)
-// const g = Math.floor(Math.random() * 255)
-// const b = Math.floor(Math.random() * 255)
-// colorTextChanger.style.color = `rgb(${r},${g},${b})`
-// } POPRAWIĆ NIE DZIAŁA
-
-
-newQuoteButton.addEventListener('click', showRandomQuotes);
-newQuoteButton.addEventListener('click', colorChanger);
+getQuote()
 
 
 
+
+
+
+
+
+
+// newQuoteButton.addEventListener('click', showRandomQuotes);
+// newQuoteButton.addEventListener('click', colorChanger);
+
+
+
+
+// const showRandomQuotes = () => {
+	// 	const number = Math.floor(Math.random() * (quotes.length - 1));
+	// 	console.log(number);
+	// 	quoteText.textContent = quotes[number];
+	
+	// };
+	
+	// const colorChange = () => {
+		// const r = Math.floor(Math.random() * 255)
+		// const g = Math.floor(Math.random() * 255)
+		// const b = Math.floor(Math.random() * 255)
+		// colorChanger.style.background = `rgb(${r},${g},${b})`
+		// }
+		
+		// const colorTextChange = () => {
+			// const r = Math.floor(Math.random() * 255)
+			// const g = Math.floor(Math.random() * 255)
+			// const b = Math.floor(Math.random() * 255)
+			// colorTextChanger.style.color= `rgb(${r},${g},${b})`
+			// }
+
+
+			// const colorTextChange = () => {
+				// const r = Math.floor(Math.random() * 255)
+				// const g = Math.floor(Math.random() * 255)
+				// const b = Math.floor(Math.random() * 255)
+				// colorTextChanger.style.color = `rgb(${r},${g},${b})`
+				// } POPRAWIĆ NIE DZIAŁA

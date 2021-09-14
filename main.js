6af73c37692290b9e84df80275fb8438d8898ff0
99mac99 <86948705+99mac99@users.noreply.github.com>
2021-09-14 17:30:20 +0200

    zmieniające sie kolory

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const newQuoteButton = document.querySelector('.button');
 const quoteText = document.querySelector('.quote-text');
 const quoteAuthor = document.querySelector('.quote-author')
 const colorChanger  = document.querySelectorAll('.color')
-
-
+const colorText = document.querySelectorAll('.colorTxt')
 
 const apiLink ='https://game-of-thrones-quotes.herokuapp.com/v1/random'
 
@@ -21,9 +20,17 @@ const colorChange = () => {
 	const r = Math.floor(Math.random() * 255)
 	const g = Math.floor(Math.random() * 255)
 	const b = Math.floor(Math.random() * 255)
-	colorChanger.style.color= `rgb(${r},${g},${b})`
+	colorChanger.forEach((el) =>(el.style.background = `rgb(${r},${g},${b})`))
+}
+
+const colorChangeText = () => {
+	const r = Math.floor(Math.random() * 255)
+	const g = Math.floor(Math.random() * 255)
+	const b = Math.floor(Math.random() * 255)
+	colorText.forEach((el) => (el.style.color = `rgb(${r},${g},${b})`))
 }
 
 newQuoteButton.addEventListener('click', getQuotes )
 newQuoteButton.addEventListener('click', colorChange)
+newQuoteButton.addEventListener('click', colorChangeText)
 

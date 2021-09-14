2703b48b27ef768431c11d96eb4b468e680257b2
99mac99 <86948705+99mac99@users.noreply.github.com>
2021-09-14 16:38:25 +0200

    zmiana kolorów

--- main.js
+++ main.js
@@ -1,60 +1,29 @@
 const newQuoteButton = document.querySelector('.button');
 const quoteText = document.querySelector('.quote-text');
 const quoteAuthor = document.querySelector('.quote-author')
-const text = document.querySelector('.text')
+const colorChanger  = document.querySelectorAll('.color')
+
+
 
 const apiLink ='https://game-of-thrones-quotes.herokuapp.com/v1/random'
 
 const getQuotes = () => {
 	axios.get(apiLink)
-		.then(res =>{ 
+		.then(res => { 
 			quoteText.textContent = res.data.sentence
 			quoteAuthor.textContent = res.data.character.name
-			console.log(res.data )
-			console.log(quotes)
-			console.log(author)
 		})
-
 }
 
+// const colorr = colorChanger.forEach(el) => 
 
-getQuotes()
+const colorChange = () => {
+	const r = Math.floor(Math.random() * 255)
+	const g = Math.floor(Math.random() * 255)
+	const b = Math.floor(Math.random() * 255)
+	colorChanger.style.color= `rgb(${r},${g},${b})`
+}
 
+newQuoteButton.addEventListener('click', getQuotes )
+newQuoteButton.addEventListener('click', colorChange)
 
-
-
-
-// newQuoteButton.addEventListener('click', showRandomQuotes);
-// newQuoteButton.addEventListener('click', colorChanger);
-
-
-
-
-// const showRandomQuotes = () => {
-	// 	const number = Math.floor(Math.random() * (quotes.length - 1));
-	// 	console.log(number);
-	// 	quoteText.textContent = quotes[number];
-	
-	// };
-	
-	// const colorChange = () => {
-		// const r = Math.floor(Math.random() * 255)
-		// const g = Math.floor(Math.random() * 255)
-		// const b = Math.floor(Math.random() * 255)
-		// colorChanger.style.background = `rgb(${r},${g},${b})`
-		// }
-		
-		// const colorTextChange = () => {
-			// const r = Math.floor(Math.random() * 255)
-			// const g = Math.floor(Math.random() * 255)
-			// const b = Math.floor(Math.random() * 255)
-			// colorTextChanger.style.color= `rgb(${r},${g},${b})`
-			// }
-
-
-			// const colorTextChange = () => {
-				// const r = Math.floor(Math.random() * 255)
-				// const g = Math.floor(Math.random() * 255)
-				// const b = Math.floor(Math.random() * 255)
-				// colorTextChanger.style.color = `rgb(${r},${g},${b})`
-				// } POPRAWIĆ NIE DZIAŁA
